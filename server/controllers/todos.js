9f1c15ad5867c33ac96c3f6dbff2dfb569c6d98d
Aditya Naik <naik.aditya@gmail.com>
2019-01-04 12:55:14 +0100

    Adds method to update a specific todo, adds route

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -41,4 +41,28 @@ module.exports = {
     })
     .catch(error => res.status(400).send(error))
   },
+
+  update(req,res){
+      return Todo
+      .findById(req.params.todoId, {
+          include: [{
+            model: TodoItem,
+            as: 'todoItems',
+          }]
+      })
+      .then(todo => {
+        if(!todo){
+            return res.status(404).send({
+                message: 'We cannot find this TODO'
+            });
+        }
+        return todo
+        .update({
+            title: req.body.title || todo.title,
+        })
+        .then(() => res.status(202).send(todo))
+        .catch(error => res.status(400).send(error));
+      })
+      .catch (error => res.status(400).send(error))
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,5 @@ module.exports = (app) => {
   app.post('/api/todos/:todoId/items', todoItemsController.create);
   app.get('/api/todos', todosController.list);
   app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
 };
