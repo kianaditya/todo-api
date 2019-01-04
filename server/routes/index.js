5b2ad7877938ba71bae3625448e32060463893bb
Aditya Naik <naik.aditya@gmail.com>
2019-01-04 10:41:41 +0100

    Creates todoItems controller, adds route and creates a post request path

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const todosController = require('../controllers').todos;
+const todoItemsController = require ('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -6,5 +7,6 @@ module.exports = (app) => {
   }));
 
   app.post('/api/todos', todosController.create);
+  app.post('/api/todos/:todoId/items', todoItemsController.create);
   app.get('/api/todos', todosController.list);
 };
