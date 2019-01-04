7b244446f628704cd4c5e33c920fb4d9d5c25b9f
Aditya Naik <naik.aditya@gmail.com>
2019-01-04 13:05:57 +0100

    Adds delete method to delete specific todo, adds corresponding route

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,4 +11,5 @@ module.exports = (app) => {
   app.get('/api/todos', todosController.list);
   app.get('/api/todos/:todoId', todosController.retrieve);
   app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
 };
